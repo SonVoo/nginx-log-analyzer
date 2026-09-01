d7d58219502ac2a6dc32bd89b2b5b461d4970ad8
Son Voo <svo70458@gmail.com>
2026-09-01 15:00:15 +0000

    them script

---
+++ nginx-analyzer.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Kiểm tra xem người dùng đã truyền vào file log chưa
+if [ -z "$1" ]; then
+    echo "Vui lòng nhập đường dẫn file log! Ví dụ: ./nginx-analyzer nginx.log"
+    exit 1
+fi
+
+LOG_FILE="$1"
+
+echo "=== TOP 5 IP ADDRESSES ==="
+awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+echo ""
+
+echo "=== TOP 5 REQUESTED PATHS ==="
+awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+echo ""
+
+echo "=== TOP 5 STATUS CODES ==="
+awk '{print $9}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+echo ""
+
+echo "=== TOP 5 USER AGENTS ==="
+awk -F'"' '{print $6}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 5
+
